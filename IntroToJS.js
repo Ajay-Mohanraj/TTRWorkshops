57cc017a12e0b8f22edeec1f9be2bbdef828568c
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2022-02-27 20:32:22 -0500

    first

--- IntroToJS.js
+++ IntroToJS.js
@@ -1,2 +1,38 @@
 // hello world workshop
-let x = 4;
+// Arithmetic operators
+
+const int = 1;
+const float = 2.3;
+const double = 3.7;
+const boolean = true;
+const char = 'w';
+const string = 'hello world';
+
+console.log(int);
+console.log(float);
+console.log(double);
+console.log(boolean);
+console.log(char);
+console.log(string);
+
+let num1 = 10;
+var num2 = 4;
+
+const addition = num1 + num2; // 10+4 = 14
+console.log(addition);
+const subtraction = num1 - num2; //10-4 = 6
+console.log(subtraction);
+const multiplication = num1 * num2; //10 *4 = 40;
+console.log(multiplication);
+const division = num1 / num2; // 10/4 = 2.5;
+console.log(division);
+
+if (num1 > num2) {
+    console.log("num1 is larger than num2.")
+}
+else if (num2 > num1) {
+    console.log("num2 is larger than num1.")
+}
+else {
+    console.log("num1 is equal to num2.")
+}
